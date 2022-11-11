156e40500fd7bc8694c1f3d5835039804d900d39
Jack Bailey <jack@jackbailey.dev>
2022-11-11 18:39:50 +0000

    Clarify ping command

--- discord/commands/ping.js
+++ discord/commands/ping.js
@@ -1,7 +1,7 @@
 const { SlashCommandBuilder } = require("discord.js");
 
 module.exports = {
-	data: new SlashCommandBuilder().setName("ping").setDescription("Replies with Pong!"),
+	data: new SlashCommandBuilder().setName("ping").setDescription("Replies with the bot's latency in ms!"),
 	async execute(interaction) {
 		const sent = await interaction.reply({ content: "Pinging...", fetchReply: true });
 		var duration = sent.createdTimestamp - interaction.createdTimestamp;
