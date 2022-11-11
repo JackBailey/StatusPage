bed9920dfe53efe2350fde69c31abe6194b71b99
Jack Bailey <jack@jackbailey.dev>
2022-11-11 00:07:48 +0000

    Add overview on DM to the bot

--- discord/index.js
+++ discord/index.js
@@ -1,14 +1,18 @@
-const { Client, Events, Collection, GatewayIntentBits } = require("discord.js");
+const { Client, Events, Collection, GatewayIntentBits, Partials, EmbedBuilder, Embed } = require("discord.js");
 const fs = require("node:fs");
 const path = require("node:path");
 const config = require("../config.json");
+const services = require("../services/index");
 require("dotenv").config();
 
 module.exports.init = async function () {
 	if (!process?.env?.TOKEN) throw new Error("Discord client token not defined!");
 	if (!config?.discord?.bot?.client_id) throw new Error("Discord client id not defined!");
 
-	const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+	const client = new Client({
+		intents: [GatewayIntentBits.Guilds, GatewayIntentBits.DirectMessages, GatewayIntentBits.GuildMessages],
+		partials: [Partials.Channel],
+	});
 	client.commands = new Collection();
 
 	// Register commands
@@ -33,6 +37,39 @@ module.exports.init = async function () {
 
 	client.once(Events.ClientReady, (c) => {
 		console.log(`Ready! Logged in as ${c.user.tag}`);
+	});
+
+	client.on(Events.MessageCreate, async (c) => {
+		if (c.guildId || c.author.bot) return;
+
+		let allServices = services.all();
+		var statuses = config.statuses;
+
+		// Destructure object of services into array
+		allServices = Object.keys(allServices).map((id) => {
+			let service = allServices[id];
+			service.id = id;
+			return service;
+		});
+
+		var embeds = Object.keys(statuses)
+			.map((id) => {
+				var status = statuses[id];
+				let servicesInStatus = allServices.filter((service) => service.status == id);
+
+				console.log(status, servicesInStatus.length);
+
+				if (servicesInStatus.length == 0) return null;
+
+				let listOfServicesInStatus = servicesInStatus.map((service) => service.title).join("\n");
+
+				console.log(listOfServicesInStatus);
+
+				return new EmbedBuilder().setTitle(status.title).setDescription(listOfServicesInStatus).setColor(`#${status.color}`);
+			})
+			.filter((embed) => embed !== null);
+
+		await c.reply({ content: "All statuses:", embeds });
 	});
 
 	// Handle interactions
